032907e3a41c9d047669ff991d9f488fd25b7528
roycms <roycms@qq.com>
2017-01-05 16:13:40 +0800

    update deme code

--- RButton/ViewController.m
+++ RButton/ViewController.m
@@ -17,22 +17,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     RButton *button = [[RButton alloc]init];
-    button.frame = CGRectMake(100.0, 150.0, 160.0, 50.0);
-    button.backgroundColor = [UIColor yellowColor];
+    button.frame = CGRectMake(100.0, 150.0, 120.0, 45.0);
+    button.backgroundColor = [UIColor blackColor];
     [button setTitle:@"点我" forState:UIControlStateNormal];
-    [button addTarget:self action:@selector(tAction:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button];
 }
-
-- (void)tAction:(id)sender {
-    NSLog(@"0000");
-
-}
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
